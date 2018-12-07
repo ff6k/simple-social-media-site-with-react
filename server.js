a19f55f22b167480494c37dc5d1fd02f2b2c0e9d
coolerwind <brzden32@gmail.com>
2018-12-07 14:45:39 -0800

    connecting MongoDB with Mongoose

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+	.connect(
+		db,
+		{ useNewUrlParser: true }
+	)
+	.then(() => console.log('MongoDB Connected'))
+	.catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello'));
 
