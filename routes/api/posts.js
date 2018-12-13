8d1313b2830621c42f457b48b91ce9060c47ae99
coolerwind <brzden32@gmail.com>
2018-12-13 12:00:55 -0800

    added like/unlike post functionality

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -106,4 +106,70 @@ router.delete('/:id', requireLogin, (req, res) => {
 		);
 });
 
+// @route   POST api/posts/like/:id
+// @desc    Like post
+// @access  Private
+router.post('/like/:id', requireLogin, (req, res) => {
+	Profile.findOne({ user: req.user.id }).then(profile => {
+		Post.findById(req.params.id)
+			.then(post => {
+				if (
+					post.likes.filter(like => like.user.toString() === req.user.id)
+						.length > 0
+				) {
+					return sender(res, 400, { error: 'User already liked this post' });
+				}
+
+				post.likes.unshift({ user: req.user.id });
+				post.save().then(post => res.json(post));
+			})
+			.catch(err =>
+				sender(res, 404, {
+					message: 'The post was not found to like.',
+					name: req.user.name,
+					userId: req.user.id,
+					postId: req.params.id,
+					error: err
+				})
+			);
+	});
+});
+
+// @route   POST api/posts/unlike/:id
+// @desc    Unlike post
+// @access  Private
+router.post('/unlike/:id', requireLogin, (req, res) => {
+	Profile.findOne({ user: req.user.id }).then(profile => {
+		Post.findById(req.params.id)
+			.then(post => {
+				if (
+					post.likes.filter(like => like.user.toString() === req.user.id)
+						.length === 0
+				) {
+					return sender(res, 400, {
+						message: 'You have not yet liked this post'
+					});
+				}
+
+				//Get remove index
+				const removeIndex = post.likes
+					.map(item => item.user.toString())
+					.indexOf(req.user.id);
+
+				//Splice out of array
+				post.likes.splice(removeIndex, 1);
+				post.save().then(post => res.json(post));
+			})
+			.catch(err =>
+				sender(res, 404, {
+					message: 'The post was not found to like.',
+					name: req.user.name,
+					userId: req.user.id,
+					postId: req.params.id,
+					error: err
+				})
+			);
+	});
+});
+
 module.exports = router;
