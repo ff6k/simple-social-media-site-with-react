088a1f9b080449ba2b71efd338a94f4ea0a95a2d
coolerwind <brzden32@gmail.com>
2018-12-14 12:37:16 -0800

    React Router setup for landing, register, and login pages

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+	render() {
+		return <div>login</div>;
+	}
+}
+export default Login;
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+	render() {
+		return <div>Register</div>;
+	}
+}
+export default Register;
