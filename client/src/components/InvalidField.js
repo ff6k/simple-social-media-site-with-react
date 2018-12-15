f7024e7e4e23063acd67e984631101736ccca9e3
coolerwind <brzden32@gmail.com>
2018-12-14 16:03:17 -0800

    adding validation to register and testing api connection

---
+++ client/src/components/InvalidField.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default props => {
+	return <div className="invalid-feedback">{props.message}</div>;
+};
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import classnames from 'classnames';
+import InvalidField from '../InvalidField';
 
 class Register extends Component {
 	state = {
@@ -7,22 +10,29 @@ class Register extends Component {
 		password: '',
 		password2: '',
 		errors: {}
-	}
+	};
 
-	onChange = e => this.setState({
-		[e.target.name]: e.target.value
-	});
+	onChange = e =>
+		this.setState({
+			[e.target.name]: e.target.value
+		});
 
-	onSubmit = e =>  {
+	onSubmit = e => {
 		e.preventDefault();
 		const newUser = {
 			...this.state
-		}
-		console.log(newUser);
-	}
+		};
+
+		axios
+			.post('/api/users/register', newUser)
+			.then(res => console.log(res.data))
+			.catch(err => this.setState({ errors: err.response.data }));
+	};
 
 	render() {
-		return <div className="register">
+		const { errors } = this.state;
+		return (
+			<div className="register">
 				<div className="container">
 					<div className="row">
 						<div className="col-md-8 m-auto">
@@ -30,29 +40,74 @@ class Register extends Component {
 							<p className="lead text-center">
 								Create your DevConnector account
 							</p>
-							<form onSubmit={this.onSubmit}>
+							<form noValidate onSubmit={this.onSubmit}>
 								<div className="form-group">
-									<input type="text" className="form-control form-control-lg" placeholder="Name" name="name" value={this.state.name} onChange={this.onChange} />
+									<input
+										type="text"
+										className={classnames('form-control form-control-lg', {
+											'is-invalid': errors.name
+										})}
+										placeholder="Name"
+										name="name"
+										value={this.state.name}
+										onChange={this.onChange}
+									/>
+									{errors.name && <InvalidField message={errors.name} />}
 								</div>
 								<div className="form-group">
-									<input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" value={this.state.email} onChange={this.onChange} />
+									<input
+										type="email"
+										className={classnames('form-control form-control-lg', {
+											'is-invalid': errors.email
+										})}
+										placeholder="Email Address"
+										name="email"
+										value={this.state.email}
+										onChange={this.onChange}
+									/>
+									{errors.email && <InvalidField message={errors.email} />}
 									<small className="form-text text-muted">
-										This site uses Gravatar so if you want a profile image,
-										use a Gravatar email
+										This site uses Gravatar so if you want a profile image, use
+										a Gravatar email
 									</small>
 								</div>
 								<div className="form-group">
-									<input type="password" className="form-control form-control-lg" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange} />
+									<input
+										type="password"
+										className={classnames('form-control form-control-lg', {
+											'is-invalid': errors.password
+										})}
+										placeholder="Password"
+										name="password"
+										value={this.state.password}
+										onChange={this.onChange}
+									/>
+									{errors.password && (
+										<InvalidField message={errors.password} />
+									)}
 								</div>
 								<div className="form-group">
-									<input type="password" className="form-control form-control-lg" placeholder="Confirm Password" name="password2" value={this.state.password2} onChange={this.onChange} />
+									<input
+										type="password"
+										className={classnames('form-control form-control-lg', {
+											'is-invalid': errors.password2
+										})}
+										placeholder="Confirm Password"
+										name="password2"
+										value={this.state.password2}
+										onChange={this.onChange}
+									/>
+									{errors.password2 && (
+										<InvalidField message={errors.password2} />
+									)}
 								</div>
 								<input type="submit" className="btn btn-info btn-block mt-4" />
 							</form>
 						</div>
 					</div>
 				</div>
-			</div>;
+			</div>
+		);
 	}
 }
 export default Register;
