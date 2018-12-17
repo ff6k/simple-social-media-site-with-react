a084c1aa14eed80b7f350f2e952dc5e168b5e674
coolerwind <brzden32@gmail.com>
2018-12-17 11:33:01 -0800

    created redux login action and set current user

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import setAuthToken from './../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
@@ -12,4 +14,33 @@ export const registerUser = (userData, history) => dispatch => {
 				payload: err.response.data
 			})
 		);
+};
+
+export const loginUser = userData => dispatch => {
+	axios
+		.post('/api/users/login', userData)
+		.then(res => {
+			//save to localStorage
+			const { token } = res.data;
+			//set token to localStorage
+			localStorage.setItem('jwtToken', token);
+			//set token to Auth header
+			setAuthToken(token);
+			const decoded = jwt_decode(token);
+			//set current user
+			dispatch(setCurrentUser(decoded));
+		})
+		.catch(
+			(err = dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			}))
+		);
+};
+
+export const setCurrentUser = decoded => {
+	return {
+		type: SET_CURRENT_USER,
+		payload: decoded
+	};
 };
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,6 @@
+import CoreValidation from './../validation/core-validation';
+import { SET_CURRENT_USER } from './../actions/types';
+
 const initialState = {
 	isAuthenticated: false,
 	user: {}
@@ -5,6 +8,11 @@ const initialState = {
 
 export default (state = initialState, action) => {
 	switch (action.type) {
+		case SET_CURRENT_USER:
+			return {
+				...state,
+				isAuthenticated: !CoreValidation.isEmpty(action.payload)
+			};
 		default:
 			return state;
 	}
