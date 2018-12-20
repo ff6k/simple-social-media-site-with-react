0708907922bb80bedbcd66ca7e63a12e157315a9
coolerwind <brzden32@gmail.com>
2018-12-20 15:46:29 -0800

    added createProfile propType

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -221,6 +221,7 @@ class CreateProfile extends Component {
 }
 
 CreateProfile.propTypes = {
+	createProfile: PropTypes.func.isRequired,
 	profile: PropTypes.object.isRequired,
 	errors: PropTypes.object.isRequired
 };
