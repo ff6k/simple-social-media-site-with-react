8a9383d67ebae8daff5e03758ef3fbac902f481d
coolerwind <brzden32@gmail.com>
2018-12-27 12:47:14 -0800

    adding profile item and display on profiles component

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -1,8 +1,53 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import CoreValidation from './../../validation/core-validation';
 
 class ProfileItem extends Component {
 	render() {
-		return <div />;
+		const { profile } = this.props;
+		return (
+			<div className="card card-body bg-light mb-3">
+				<div className="row">
+					<div className="col-2">
+						<img alt="" className="rounded-circle" src={profile.user.avatar} />
+					</div>
+					<div className="col-lg-6 col-md-4 col-8">
+						<h3>{profile.user.namem}</h3>
+						<p>
+							{profile.status}{' '}
+							{CoreValidation.isEmpty(profile.company) ? null : (
+								<span>at {profile.company}</span>
+							)}
+						</p>
+						<p>
+							{CoreValidation.isEmpty(profile.location) ? null : (
+								<span>{profile.location}</span>
+							)}
+						</p>
+						<Link className="btn btn-info" to={`/profile/${profile.handle}`}>
+							View Profile
+						</Link>
+					</div>
+					<div className="col-md-4 d-none d-md-block">
+						<h4>Skill set</h4>
+						<ul className="list-group">
+							{profile.skills.slice(0, 4).map((skill, index) => (
+								<li className="list-group-item" key={index}>
+									<i className="fa fa-check pr-1" />
+									{skill}
+								</li>
+							))}
+						</ul>
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
+
+ProfileItem.propTypes = {
+	profile: PropTypes.object.isRequired
+};
+
 export default ProfileItem;
--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getProfiles } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
+import ProfileItem from './ProfileItem';
 
 class Profiles extends Component {
 	componentDidMount() {
@@ -16,7 +17,9 @@ class Profiles extends Component {
 			profileItems = <Spinner />;
 		} else {
 			if (profiles.length > 0) {
-				profileItems = <h1>PROFILES HERE</h1>;
+				profileItems = profiles.map(profile => (
+					<ProfileItem key={profile._id} profile={profile} />
+				));
 			} else {
 				profileItems = <h4>No profiles found...</h4>;
 			}
